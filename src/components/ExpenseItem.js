5a19135750d4bd23d2925e4229f27d27c62322b5
sumeetmishra1 <sumitsfs0@gmail.com>
2024-01-20 15:52:42 +0530

    Added js variable in componentrs

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,18 @@
+import './ExpenseItem.css'
 function  ExpenseItem(){
-    return <h2>ExpenseItem</h2>
+    const expenseDate =new Date().toISOString();
+    const expenseName= 'Car fees'
+    const location ='Gorakhpur'
+    const expenseAmount=1000
+    return (
+      <div className="expense-item">
+        <div>{expenseDate}</div>
+        <div className="expense-item__description">
+          <h2>{expenseName}</h2>
+          <div className="expense-item__price">{expenseAmount}</div>
+          <div>{location}</div>
+        </div>
+      </div>
+    );
 }
 export default ExpenseItem
