0571018c724543818062db93aa438b84748a0452
sumeetmishra1 <sumitsfs0@gmail.com>
2024-01-21 14:09:04 +0530

    event listener(delete button)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,11 +4,15 @@ import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 function  ExpenseItem(props){
     const expenseDate =props.expense.date
-   
+   function deleteExpense(e){
+    e.target.parentElement.remove()
+    console.log()
+   }
     return (
       <Card className="expense-item">
         <ExpenseDate date={expenseDate}/>
         <ExpenseDescription expense={props.expense}/>
+        <button onClick={deleteExpense}>Delete</button>
       </Card>
      
     );
